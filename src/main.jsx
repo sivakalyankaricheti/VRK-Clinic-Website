a348287d37383582e3dff20ce85f5cc92df3eb86
Siva Kalyan <kalyankaricheti@gmail.com>
2026-08-03 12:58:20 +0200

    Refresh VRK Clinic UI and UX design

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import './brand-overrides.css';
+import './ui-refresh.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
